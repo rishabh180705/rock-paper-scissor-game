6de52f0b988112afc4f8223ab421239bad2c812b
rishabh180705 <146173389+rishabh180705@users.noreply.github.com>
2024-04-27 02:44:47 +0530

    commit it

---
+++ logic.js
@@ -0,0 +1,70 @@
+let userScore=0;
+let compScore=0;
+const choices=document.querySelectorAll(".component");
+
+let resetbtn=document.querySelector("#reset");
+
+let msg=document.querySelector("#msgbox");
+
+const draw=()=>{
+    msg.innerHTML=`Game draw`;
+}
+const compGenChoice=()=>{
+    const options=["rock","scissors","paper"]
+    let num=Math.floor(Math.random()*3);
+    return options[num];
+}
+
+const displaymsg=(userWin,userChoice,compChoice)=>{
+      if(userWin){
+        msg.innerHTML=`you win! ${userChoice} beats ${compChoice}`;
+      }
+      else{
+        msg.innerHTML=`you lose ${compChoice} beats ${userChoice}`;
+      }
+}
+ const playGame=(userChoice)=>{
+   let compChoice= compGenChoice();
+console.log(compChoice);
+console.log(userChoice);
+   if(userChoice===compChoice){
+    draw();
+   }
+   else{
+    let userWin=true;
+    if(userChoice==="rock"){
+        //paper scissors
+        userWin=compChoice==="paper"?false:true;
+    }
+    else if(userChoice=="paper"){
+        userWin=compChoice==="rock"?true:false;
+    }
+    else{
+        userWin=compChoice=="rock"?false:true;
+    }
+
+    if(userWin) userScore++;
+    else compScore++;
+    let user=document.getElementById("user");
+    let comp=document.getElementById("comp");
+
+    displaymsg(userWin,userChoice,compChoice);
+    user.innerHTML=userScore;
+    comp.innerHTML=compScore;
+         resetbtn.addEventListener("click",()=>{
+            user.innerHTML=0;
+            comp.innerHTML=0;
+            userScore=0;
+            compScore=0;
+            msg.innerHTML="play the game";
+         });
+   }
+
+ }
+
+choices.forEach((choice) =>{
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+         playGame(userChoice);
+    });
+});
